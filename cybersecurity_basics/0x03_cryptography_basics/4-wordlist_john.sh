1246e6be6b2e56920f97df70159872e22bdcac09
GomesCabral <phgcabral8@gmail.com>
2026-05-18 14:13:19 +0200

    add 4-wordlist_john.sh 4-password.txt files

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "$(john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-md5 "$1" 2>/dev/null)$(john --show --format=raw-md5 "$1" | cut -d ":" -f2 | head -n -2)" > 4-password.txt
+john --show --format=raw-md5 "$1" | cut -d ":" -f2 | head -n -2 > 4-password.txt
