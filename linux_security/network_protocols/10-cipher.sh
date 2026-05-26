f6c246c83f4cb2e8b5bc4dc75a12912682ace61a
GomesCabral <phgcabral8@gmail.com>
2026-05-26 16:20:53 +0200

    fix 10-cipher.sh file

--- linux_security/network_protocols/10-cipher.sh
+++ linux_security/network_protocols/10-cipher.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-TARGET="$1"
-
-if [ -z "$TARGET" ]; then
-  echo "Usage: sudo ./10-cipher.sh <target-ip-or-domain>"
-  exit 1
-fi
-
-nmap --script ssl-enum-ciphers -p 443 "$TARGET"
+nmap --script ssl-enum-ciphers -p 443 "$1"
